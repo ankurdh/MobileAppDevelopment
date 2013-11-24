26e5c884fbd8ac07c4cdefaf5656b85fcf191b1b
Ankur Huralikoppi <ahuralik@uncc.edu>
2013-11-24 14:32:24 -0500

    -Adding File Metadata
    Added author name in MainActivity.java in javadocs style

--- Final Project/src/edu/uncc/mad/huduku/MainActivity.java
+++ Final Project/src/edu/uncc/mad/huduku/MainActivity.java
@@ -1,3 +1,7 @@
+/**
+* @Author: Ankur Huralikoppi
+*/
+
 package edu.uncc.mad.huduku;
 
 import java.util.ArrayList;
@@ -31,30 +35,30 @@ import edu.uncc.mad.huduku.providers.YelpBusinessProvider;
 import edu.uncc.mad.huduku.providers.YelpSearchProvider;
 
 public class MainActivity extends Activity implements LocationChangeObserver {
-	
+
 	private Handler handler;
 	private ExecutorService es;
-	
+
 	private ProgressDialog dialog;
 //	private LocationObservable locationProvider;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		/**
 		 * Initialize the Pinned places handler module
 		 */
-		SharedPreferenceManager.createInstance(getApplication(), getString(R.string.PINNED_PLACES_FILE_NAME));	
-		
+		SharedPreferenceManager.createInstance(getApplication(), getString(R.string.PINNED_PLACES_FILE_NAME));
+
 		/**
-		 * USAGE: 
+		 * USAGE:
 		 * SharedPreferenceManager manager = SharedPreferenceManager.getSharedPreferenceManager();
 		 */
-		
+
 		es = Executors.newFixedThreadPool(1);
-		
+
 		dialog = new ProgressDialog(MainActivity.this);
 		dialog.setCancelable(false);
 		dialog.setMessage("Getting data from internet");
@@ -64,14 +68,14 @@ public class MainActivity extends Activity implements LocationChangeObserver {
 			@Override
 			synchronized public boolean handleMessage(Message msg) {
 				dialog.dismiss();
-				
+
 //				ArrayList<Restaurant> restaurants = msg.getData().getParcelableArrayList("RESTAURANTS");
 				//((TextView)findViewById(R.id.restaurantsCount)).setText(restaurants.get(2).getDeals().get(0).getBuyUrl());
-				
+
 				return false;
 			}
 		});
-		
+
 		onLocationChanged(35.306993, -80.723481);
 //		onLocationChanged(35.231006,-80.839666);
 //		onLocationChanged(35.22569,-80.838336);
@@ -95,24 +99,24 @@ public class MainActivity extends Activity implements LocationChangeObserver {
 }
 
 class CityGridProvider implements Runnable {
-	
+
 	private double lat;
 	private double lon;
 	private Handler handler;
-	
+
 	public CityGridProvider(Handler handler, double lat, double lon ){
 		this.lat = lat;
 		this.lon = lon;
 		this.handler = handler;
 	}
-	
+
 	@Override
 	public void run(){
 		CityGrid cg = new CityGrid();
 		String restaurantsJSONString = cg.placesSearch(lat, lon);
-		CityGridJSONParserImpl parser = new CityGridJSONParserImpl(); 
+		CityGridJSONParserImpl parser = new CityGridJSONParserImpl();
 		List<Restaurant> restaurants = parser.getRestaurantsFrom(restaurantsJSONString);
-		
+
 		for(Restaurant r: restaurants){
 			try {
 				r.setReviews(parser.getReviewsFrom(new JSONObject(cg.reviewsSearch(r.getId()))));
@@ -120,23 +124,23 @@ class CityGridProvider implements Runnable {
 				e.printStackTrace();
 			}
 		}
-		
+
 		Message msg = new Message();
 		Bundle bundle = new Bundle();
-		
+
 		bundle.putParcelableArrayList("RESTAURANTS", (ArrayList<Restaurant>) restaurants);
 		msg.setData(bundle);
-		
+
 		handler.sendMessage(msg);
 	}
 }
 
 class YelpProvider implements Runnable {
-	
-	private Handler handler;	
+
+	private Handler handler;
 	private double latitude;
 	private double longitude;
-	
+
 	public YelpProvider(Handler handler, double latitude, double longitude){
 		this.handler = handler;
 		this.latitude = latitude;
@@ -146,49 +150,49 @@ class YelpProvider implements Runnable {
 	@Override
 	public void run() {
 		String searchJSONResponse = new YelpSearchProvider(latitude, longitude).getSearchResults();
-		
+
 		List<Restaurant> restaurants = new YelpJSONParserImpl().getRestaurantsFrom(searchJSONResponse);
-		YelpBusinessProvider yelpBusinessProvider; 
-		
+		YelpBusinessProvider yelpBusinessProvider;
+
 		for(Restaurant r: restaurants){
 			yelpBusinessProvider = new YelpBusinessProvider(r);
 			r = yelpBusinessProvider.getRestaurantReviews();
 			Log.d("ankur", "YELP - Name: " + r.getName());
 		}
-		
+
 		Message msg = new Message();
 		Bundle bundle = new Bundle();
-		
+
 		bundle.putParcelableArrayList("RESTAURANTS", (ArrayList<Restaurant>) restaurants);
 		msg.setData(bundle);
-		
+
 		handler.sendMessage(msg);
-		
+
 	}
 }
 
 
 class GoogleProvider implements Runnable {
-	
-	private Handler handler;	
+
+	private Handler handler;
 	private double latitude;
 	private double longitude;
-	
+
 	public GoogleProvider(Handler handler, double latitude, double longitude){
 		this.handler = handler;
 		this.latitude = latitude;
-		this.longitude = longitude;	
+		this.longitude = longitude;
 	}
 
 	@Override
 	public void run() {
 		GooglePlacesProvider googlePlacesProvider = new GooglePlacesProvider();
 		GooglePlacesJSONParserImpl googleJSONParser = new GooglePlacesJSONParserImpl();
-		
+
 		String searchJSONResponse = googlePlacesProvider.getGoogleSearchRestaurants(latitude, longitude);
-		
+
 		List<Restaurant> restaurants = googleJSONParser.getRestaurantsFrom(searchJSONResponse);
-				
+
 		for(Restaurant r: restaurants){
 			Log.d("ankur", "Google Restaurant: " + r.getName());
 			try {
@@ -197,14 +201,14 @@ class GoogleProvider implements Runnable {
 				e.printStackTrace();
 			}
 		}
-		
+
 		Message msg = new Message();
 		Bundle bundle = new Bundle();
-		
+
 		bundle.putParcelableArrayList("RESTAURANTS", (ArrayList<Restaurant>) restaurants);
 		msg.setData(bundle);
-		
+
 		handler.sendMessage(msg);
-		
+
 	}
 }
